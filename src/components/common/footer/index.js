efbf9dffa3f49eac24dfacce1cd0101457a9c405
hemu789 <87188130+hemu789@users.noreply.github.com>
2023-01-31 22:36:19 +0530

    Fix
    Fix

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made By 🔥<b> Hemant, Kunal & Manish</b></div>
+  );
+};
+
+export default Footer;
